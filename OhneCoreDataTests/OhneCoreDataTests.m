b9fab8874db2f03d962ae85fbe47130ced80440b
frankregel <frankregel@Franks-Mac-mini.local>
2014-05-09 13:32:49 +0200

    Initial Commit

---
+++ OhneCoreDataTests/OhneCoreDataTests.m
@@ -0,0 +1,34 @@
+//
+//  OhneCoreDataTests.m
+//  OhneCoreDataTests
+//
+//  Created by Frank Regel on 09.05.14.
+//  Copyright (c) 2014 appanatics. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OhneCoreDataTests : XCTestCase
+
+@end
+
+@implementation OhneCoreDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
